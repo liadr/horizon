2b897872e4d407a8e4ee2e3251fcfc3f8a96a386
Raghu Betina <raghu@firstdraft.com>
2018-03-02 22:43:10 +0000

    Validate presence of reviews.body

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,6 +5,8 @@ class Review < ApplicationRecord
 
   # Validations
 
+  validates :body, :presence => true
+
   validates :num_stars, :presence => true
 
   validates :num_stars, :inclusion => { :in => [ '1', '2', '3', '4', '5' ]  }
