86efc4902d63d3ffeb514b01efe0ffc9bf38daf0
Raghu Betina <raghu@firstdraft.com>
2018-03-02 22:43:54 +0000

    Whitelist columns in Reservation for ActiveAdmin

--- app/admin/reservations.rb
+++ app/admin/reservations.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Reservation do
+
+ permit_params :user_id, :listing_id, :status
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
