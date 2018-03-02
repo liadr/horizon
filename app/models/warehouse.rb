5762d0b2d14e74ef6709a26e4967435746660ff0
Raghu Betina <raghu@firstdraft.com>
2018-03-02 22:43:10 +0000

    Validate inclusion of warehouses.staffed

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -5,6 +5,8 @@ class Warehouse < ApplicationRecord
 
   # Validations
 
+  validates :staffed, :inclusion => { :in => [ 'Yes', 'No' ]  }
+
   validates :storage_methods, :presence => true
 
   validates :storage_methods, :inclusion => { :in => [ 'Double Stacked', 'Single Stacked' ]  }
