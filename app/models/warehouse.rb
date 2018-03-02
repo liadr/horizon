34a4def8619240f3cf8b124af28b0c4991b717bd
Raghu Betina <raghu@firstdraft.com>
2018-03-02 22:43:10 +0000

    Validate presence of warehouses.storage_methods

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -5,6 +5,8 @@ class Warehouse < ApplicationRecord
 
   # Validations
 
+  validates :storage_methods, :presence => true
+
   validates :storage_methods, :inclusion => { :in => [ 'Double Stacked', 'Single Stacked' ]  }
 
 end
