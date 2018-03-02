0811fe2055781db55c2fe7eef582554a89154faa
Raghu Betina <raghu@firstdraft.com>
2018-03-02 22:42:54 +0000

    Generate states resource with starter:resource

---
+++ app/controllers/states_controller.rb
@@ -0,0 +1,79 @@
+class StatesController < ApplicationController
+  def index
+    @states = State.all
+
+    render("states/index.html.erb")
+  end
+
+  def show
+    @state = State.find(params[:id])
+
+    render("states/show.html.erb")
+  end
+
+  def new
+    @state = State.new
+
+    render("states/new.html.erb")
+  end
+
+  def create
+    @state = State.new
+
+    @state.name = params[:name]
+
+    save_status = @state.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/states/new", "/create_state"
+        redirect_to("/states")
+      else
+        redirect_back(:fallback_location => "/", :notice => "State created successfully.")
+      end
+    else
+      render("states/new.html.erb")
+    end
+  end
+
+  def edit
+    @state = State.find(params[:id])
+
+    render("states/edit.html.erb")
+  end
+
+  def update
+    @state = State.find(params[:id])
+
+    @state.name = params[:name]
+
+    save_status = @state.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/states/#{@state.id}/edit", "/update_state"
+        redirect_to("/states/#{@state.id}", :notice => "State updated successfully.")
+      else
+        redirect_back(:fallback_location => "/", :notice => "State updated successfully.")
+      end
+    else
+      render("states/edit.html.erb")
+    end
+  end
+
+  def destroy
+    @state = State.find(params[:id])
+
+    @state.destroy
+
+    if URI(request.referer).path == "/states/#{@state.id}"
+      redirect_to("/", :notice => "State deleted.")
+    else
+      redirect_back(:fallback_location => "/", :notice => "State deleted.")
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the State resource:
+  # CREATE
+  get "/states/new", :controller => "states", :action => "new"
+  post "/create_state", :controller => "states", :action => "create"
+
+  # READ
+  get "/states", :controller => "states", :action => "index"
+  get "/states/:id", :controller => "states", :action => "show"
+
+  # UPDATE
+  get "/states/:id/edit", :controller => "states", :action => "edit"
+  post "/update_state/:id", :controller => "states", :action => "update"
+
+  # DELETE
+  get "/delete_state/:id", :controller => "states", :action => "destroy"
+  #------------------------------
+
   # Routes for the Photo resource:
   # CREATE
   get "/photos/new", :controller => "photos", :action => "new"
