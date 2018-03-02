5b6e513ac6397bd0d1593e4704a330ceebff2218
Raghu Betina <raghu@firstdraft.com>
2018-03-02 22:43:27 +0000

    Direct association between warehouses and reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,8 @@
 class Review < ApplicationRecord
   # Direct associations
 
+  belongs_to :warehouse
+
   belongs_to :user,
              :counter_cache => true
 
--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,6 +1,9 @@
 class Warehouse < ApplicationRecord
   # Direct associations
 
+  has_many   :reviews,
+             :dependent => :destroy
+
   has_many   :listings,
              :dependent => :destroy
 
